151128e1ea1930a76b1f45d3c3c03fb3298775cd
valeserber <valserber@gmail.com>
2019-08-01 11:51:28 -0300

    Navigation & UI foundations (#1)
    * Adding project structure and navigation dependencies
    
    * Adding Fragments and navigation
    
    * Creating layouts

--- app/src/main/java/com/valeserber/githubchallenge/ui/MainActivity.kt
+++ app/src/main/java/com/valeserber/githubchallenge/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.valeserber.githubchallenge
+package com.valeserber.githubchallenge.ui
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.valeserber.githubchallenge.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +10,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
